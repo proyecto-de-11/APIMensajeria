8384b5a93f3e975ea2da3073373091a88f7c131a
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-11-12 19:12:47 -0600

    creando chat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule, ChatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
