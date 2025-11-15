5079b983af46c77770bb900997871592538a0b07
Alan Campos <cs25001@esfe.agape.edu.sv>
2025-11-15 02:31:59 +0000

    instalando

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 
 import { ChatsModule } from './chats/chats.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ChatsModule],
+  imports: [ ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-test-esfe-8ff0.b.aivencloud.com',
+      port: 20787,
+      username: 'avnadmin',
+      password: 'AVNS_PfxZhqEHsyFEn50ylaO',
+      database: 'TiendaOnline',
+      logging: true,
+      synchronize: false,
+      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    })
+    ,ChatsModule],
   providers: [AppService],
 })
 export class AppModule {}
